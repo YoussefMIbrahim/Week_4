d9a04883a70d2077d5fb186f0f30190ac043b15f
Buster Scruggs <youssefibrahim909@gmail.com>
2024-07-28 18:40:51 -0500

    added implement

---
+++ src/ImmutableList.java
@@ -0,0 +1,16 @@
+import ClientInterface.IImmutableList;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImmutableList implements IImmutableList{
+
+    private List<Integer> list;
+
+    public ImmutableList(int[] array){
+        
+        List<Integer> list =  new ArrayList<>();
+
+    }
+
+
+}
